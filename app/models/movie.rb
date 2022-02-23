e179b30316a3c761ff8e4b080addc4ff687ca584
EC2 Default User <ec2-user@ip-172-31-71-4.ec2.internal>
2022-02-23 04:45:25 +0000

    test1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,18 @@
 class Movie < ActiveRecord::Base
+    	def self.all_ratings
+		#	['G','PG','PG-13','R']
+		@@all_ratings = ['G','PG','PG-13','R']
+		@@all_ratings	
+	end
+
+	def self.with_ratings(ratings_list)
+		filtered_movies = nil
+		if ratings_list == nil
+			filtered_movies = self.all
+		else
+			filtered_movies = self.where(rating: ratings_list)
+		end
+		filtered_movies
+	end
+
 end
