d4144281c6cf8eb2866e7d688347cd61b4f489d6
EC2 Default User <ec2-user@ip-172-31-71-4.ec2.internal>
2022-02-26 01:30:13 +0000

    test1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,18 +11,18 @@ class MoviesController < ApplicationController
     @rating_check_box = @uniq_movie_ratings
     
         
-    if params.key?(:movie_sort)
-      session[:movie_sort] = params[:movie_sort]
+    if params.key?( :movie_sort )
+        session[:movie_sort ] = params[:movie_sort ]
     end
     
-    if params.key?(:ratings)
-      session[:ratings] = params[:ratings].keys
+    if params.key?( :ratings )
+        session[ :ratings ] = params[ :ratings ].keys
     end
     
     
-    if session.key?(:ratings)
-      @rating_check_box = session[:ratings]
-      @movies = @movies.where(rating: @rating_check_box)
+    if session.key?( :ratings )
+      @rating_check_box = session[ :ratings ]
+      @movies = @movies.where( rating: @rating_check_box )
     end
     
     
